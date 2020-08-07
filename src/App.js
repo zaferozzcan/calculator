8146ee14f4f94cfc657701117007c92ed6430413
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-08-06 23:45:20 -0400

    Add row classes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+// import "./styles.scss";
+
+const App = () => (
+  <div className="Calculator-main">
+    <form>
+      <div className="Calculator-screen">
+        <input className="result" type="text" placeholder="0" name="result" />
+      </div>
+      <div className="Calculator-buttons">
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+      </div>
+    </form>
+  </div>
+);
+export default App;
